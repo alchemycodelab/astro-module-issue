46105f88f4c11b4df6d598e68ac79f9f70582408
Marty Nelson <marty@martypdx.com>
2022-04-28 11:14:19 -0700

    add PORT to config for heroku

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
+const port = Number(process.env.PORT) || 3000;
+
+console.log('starting astro server on', PORT);
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
-});
+  integrations: [react()],
+  server: { port, host: true, }
+} );
